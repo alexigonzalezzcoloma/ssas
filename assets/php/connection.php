9e9667a5e070009ab9dd528da58acaf84bc96daf
alexigonzalezzcoloma <alexi.gonzalez.coloma@gmail.com>
2025-04-04 18:20:01 -0300

    Update connection.php

--- assets/php/connection.php
+++ assets/php/connection.php
@@ -1,8 +1,8 @@
 <?php 
 		function connection(){
-			$servidor="localhost";
+			$servidor="ballast.proxy.rlwy.net:30044/railway";
 			$usuario="root";
-			$password="";
+			$password="gtnkMyMmOlZzTJqZlugyCjUvZubTYKmZ";
 			$bd="solicitudes";
 
 			$connection=mysqli_connect($servidor,$usuario,$password,$bd);
